09839a7e16a36502740cc729ef51c4712ae6052b
moxin <425420437@qq.com>
2019-03-20 23:28:30 +0800

    Default Changelist

---
+++ src/main/java/com/mmall/controller/portal/UserSpringSessionController.java
@@ -0,0 +1,90 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisShardedPoolUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @auther lyd
+ * @createDate 2019/3/19 21:20
+ */
+@Controller
+@RequestMapping("/user/springsession")
+public class UserSpringSessionController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    /**
+     *用户登录
+     * @param userName
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "/login.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String userName, String password, HttpSession session){
+        ServerResponse<User> response=iUserService.login(userName,password);
+
+        if (response.isSuccess()){
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+            //CookieUtil.writeLoginToken(httpServletResponse,session.getId());
+            //CookieUtil.readLoginToken(request);
+            //RedisShardedPoolUtil.setEx(session.getId(), Const.RedisCacheExtime.REDIS_SESSION_EXTIME, JsonUtil.obj2String(response.getData()));
+        }
+        return response;
+    }
+
+    /**
+     * 用户退出
+     * @return
+     */
+    @RequestMapping(value = "logout.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session){
+        //String loginToken = CookieUtil.readLoginToken(request);
+        //CookieUtil.delLoginToken(request, response);
+        //RedisShardedPoolUtil.del(loginToken);
+
+        session.removeAttribute(Const.CURRENT_USER);
+        return ServerResponse.createBySuccess();
+    }
+
+    /**
+     * 获取登录用户信息
+     * @param httpServletRequest
+     * @return
+     */
+    @RequestMapping(value = "get_user_info.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session, HttpServletRequest httpServletRequest){
+
+        /*String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+        if(StringUtils.isEmpty(loginToken)){
+            return ServerResponse.createByERROR("用户未登录,无法获取当前用户的信息");
+        }
+        String userJsonStr = RedisShardedPoolUtil.get(loginToken);
+        User user = JsonUtil.string2Obj(userJsonStr,User.class);*/
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+
+        if(user != null){
+            return ServerResponse.createBySuccess(user);
+        }
+        return ServerResponse.createByERROR("用户未登录,无法获取当前用户的信息");
+    }
+}
--- src/main/java/com/mmall/pojo/User.java
+++ src/main/java/com/mmall/pojo/User.java
@@ -2,10 +2,11 @@ package com.mmall.pojo;
 
 import lombok.ToString;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @ToString
-public class User {
+public class User implements Serializable {
     private Integer id;
 
     private String username;
