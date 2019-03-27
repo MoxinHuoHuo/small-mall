8855fd45c846200d44305e2c5d7f0c38283818b9
moxin <425420437@qq.com>
2019-03-27 20:14:21 +0800

    Default Changelist

--- src/main/java/com/mmall/common/RedissonManager.java
+++ src/main/java/com/mmall/common/RedissonManager.java
@@ -19,44 +19,43 @@ public class RedissonManager {
 
     //在redis环境没有搭建起来之前，这里先注释上，否则项目启动不起来。
 
+    // 声明一个Config类，用于存放连接redis配置
     private Config config = new Config();
 
-        private Redisson redisson = null;
+    private Redisson redisson = null;
 
-        private static final String redis1Ip = PropertiesUtil.getProperty("redis1.ip");
-        private static String  redisPassword1 = PropertiesUtil.getProperty("redis1.password");
-        private static final Integer redis1Port = Integer.parseInt(PropertiesUtil.getProperty("redis1.port"));
+    private static final String redis1Ip = PropertiesUtil.getProperty("redis1.ip");
+    private static String redisPassword1 = PropertiesUtil.getProperty("redis1.password");
+    private static final Integer redis1Port = Integer.parseInt(PropertiesUtil.getProperty("redis1.port"));
     private static Integer redisTimeOut = Integer.parseInt(PropertiesUtil.getProperty("redis.timeout"));
 
 
+    //注入到Spring容器的话，使用@PostConstruct或者静态块初始化，效果是一样的{}
 
-        //注入到Spring容器的话，使用@PostConstruct或者静态块初始化，效果是一样的{}
+    @PostConstruct
+    private void init() {
+        try {
+            //在redis环境没有搭建起来之前，这里先注释上，否则项目启动不起来。
 
+            log.info("初始化Redisson开始！");
 
-        @PostConstruct
-        private void init() {
-            try {
-                //在redis环境没有搭建起来之前，这里先注释上，否则项目启动不起来。
-
-                //127.0.0.1:6379
+            // 使用单个Redis
             config.useSingleServer().setAddress(new StringBuilder().append(redis1Ip).append(":").append(redis1Port).toString())
                     .setPassword(redisPassword1).setTimeout(redisTimeOut);
 
-
-                //单主模式
+            //单主模式
 //            config.useMasterSlaveServers().setMasterAddress(new StringBuilder().append(redis1Ip).append(":").append(redis1Port).toString());
 
-
-                //主从模式
+            //主从模式
 //            config.useMasterSlaveServers().setMasterAddress("10.211.55.6:6379").addSlaveAddress("10.211.55.6:6380");
 
-
+            // 创建Redisson
             redisson = (Redisson) Redisson.create(config);
-                log.info("初始化Redisson结束");
-            } catch (Exception e) {
-                log.error("redisson init error", e);
-            }
+            log.info("初始化Redisson结束！");
+        } catch (Exception e) {
+            log.error("redisson init error！", e);
         }
+    }
 
 
 //    {
@@ -64,8 +63,7 @@ public class RedissonManager {
 //    }
 
 
-
-        public Redisson getRedisson() {
-            return redisson;
-        }
+    public Redisson getRedisson() {
+        return redisson;
+    }
 }
